fb28546183a1d510047701e7f6be6e96ac956bc2
Megan Korling <korlingm@gmail.com>
2023-01-14 14:55:42 -0800

    Load default data for New York

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -2,27 +2,48 @@ import axios from "axios";
 import "../styles/App.css";
 import CityInformation from "./CityInformation";
 import Forecast from "./Forecast";
+import { useEffect, useState } from "react";
 
 function App() {
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-  const getLocationInformation = ({ zip }) => {
+  const [weatherData, setWeatherData] = useState({});
+
+  const getLocationInformation = (zip) => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`
       )
       .then((response) => {
         let data = response.data;
+        setWeatherData({
+          description: data["weather"][0]["main"],
+          temp: data["main"]["temp"],
+          temp_high: data["main"]["temp_max"],
+          temp_low: data["main"]["temp_min"],
+          location: data["name"],
+          wind: data["wind"]["speed"],
+          sunrise: data["sys"]["sunrise"],
+          sunset: data["sys"]["sunset"],
+          pressure: data["main"]["pressure"],
+          humidity: data["main"]["humidity"],
+        });
+        console.log(weatherData);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
+  useEffect(() => {
+    getLocationInformation("10036");
+  }, []);
+
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <CityInformation></CityInformation>
+      <h2>{weatherData.location}</h2>
+      <CityInformation weather={weatherData}></CityInformation>
       <Forecast></Forecast>
     </div>
   );
--- weather-app/src/components/CityInformation.js
+++ weather-app/src/components/CityInformation.js
@@ -1,20 +1,18 @@
-import PropTypes from "prop-types";
 import ZipForm from "./ZipForm";
 import ToggleDisplay from "./ToggleDisplay";
 
-const CityInformation = (props) => {
+const CityInformation = ({ weather }) => {
   return (
     <section>
-      <h2>Current Weather... in "New York"</h2>
+      <h2>Current weather in... {weather.location}</h2>
       <ul>
-        <li>Sky</li>
-        <li>Temp</li>
-        <li>Location</li>
-        <li>Today's high</li>
-        <li>Today's low</li>
+        <li>{weather.description}</li>
+        <li>Current Temperature: {weather.temp}</li>
+        <li>Today's High: {weather.temp_high}</li>
+        <li>Today's Low: {weather.temp_low}</li>
       </ul>
       <ZipForm></ZipForm>
-      <ToggleDisplay></ToggleDisplay>
+      <ToggleDisplay weather={weather}></ToggleDisplay>
     </section>
   );
 };
